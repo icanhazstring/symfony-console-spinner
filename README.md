c7dc1a5da4bfaa891fd3cf1e6b1021d57091dbb7
Andreas Frömer <blubb0r05+github@gmail.com>
2020-02-14 17:44:50 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # symfony-console-spinner
-Custom symfony progressbar displaying a spinner
+Custom symfony progressbar with a spinner.
+
+I wanted a progressbar displaying a long running task like
+waiting for an external resource to be available. This is where I
+stumbled upon [alecrabbit/php-cli-snake](https://github.com/alecrabbit/php-cli-snake).
+
+I tried to mimik the the animation using symfony/console and this
+is the result:
+
+![](docs/spinnerprogress.gif)
+
+## Installation
+
+Use composer to require the small package
+
+    $ composer require icanhazstring/symfony-console-spinner
+    
+## Usage
+
+To use the spinner, just instantiate the `SpinnerProgress` like the
+default `ProgressBar` from symfony giving it the `OutputInterface` and
+the maximum count of items to process:
+
+    public function execute(OutputInterface $output, InputInterface $input)
+    {
+        $spinner = new SpinnerProgress($output, 100);
+        
+        for($i = 0; $i < 100; $i++) {
+            usleep(1000);
+            $spinner->advance();
+        }
+        
+        $spinner->finish();
+    } 
+    
+   
+## License
+
+This package is released under the [MIT license](LICENSE).
---
+++ src/SpinnerProgress.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace icanhazstring\SymfonyConsoleSpinner;
+
+use Symfony\Component\Console\Helper\ProgressBar;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class SpinnerProgress
+{
+    private const CHARS = ['⠏', '⠛', '⠹', '⢸', '⣰', '⣤', '⣆', '⡇'];
+
+    /**
+     * @var ProgressBar
+     */
+    private $progressBar;
+    /**
+     * @var int
+     */
+    private $step;
+
+    public function __construct(OutputInterface $output, int $max = 0)
+    {
+        $this->progressBar = new ProgressBar($output->section(), $max);
+        $this->progressBar->setBarCharacter('✔');
+        $this->progressBar->setFormat('%bar%  %message%');
+        $this->progressBar->setBarWidth(1);
+        $this->progressBar->setRedrawFrequency(31);
+
+        $this->step = 0;
+    }
+
+    public function advance(int $step = 1): void
+    {
+        $this->step += $step;
+        $this->progressBar->setProgressCharacter(self::CHARS[$this->step % 8]);
+        $this->progressBar->advance($step);
+    }
+
+    public function setMessage(string $message): void
+    {
+        $this->progressBar->setMessage($message, 'message');
+    }
+
+    public function finish(): void
+    {
+        $this->progressBar->finish();
+    }
+}
